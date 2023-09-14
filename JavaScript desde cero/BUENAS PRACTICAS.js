ed7a3e954f2aa06f998c9a697b547747c9257ab8
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2023-09-14 16:04:18 -0600

    javascript desde cero

---
+++ JavaScript desde cero/BUENAS PRACTICAS.js
@@ -0,0 +1,22 @@
+/*
+Algunas buenas practicas a la hora de usar js pueden ser:
+
+Usar snake_cas en:
+ Archivos: mi_archivo_javascript.js
+
+ Usar UPPED_CASE en:
+ Constantes:  const UNA_CONSTANTE =
+             "soy una constante", PI= 3.141592
+
+Usar UperCamelCase en:
+ Clases:
+
+     class SerHumano {
+        constructor (nombre, genero){
+            this.nombre = nombre
+            this.genero = genero
+        }
+     }
+
+
+*/
