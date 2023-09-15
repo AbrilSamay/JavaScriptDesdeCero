f87dfbc30d9c97c4f3821437e5e1060d1cfb870d
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2023-09-14 20:07:33 -0600

    Var let y const

---
+++ JavaScript desde cero/tipos de datos/Let, var y const/Var let y const.js
@@ -0,0 +1,43 @@
+/*
+Var let y const
+---------------
+
+
+VAR
+---
+
+Usar var es una muy mala practica debido que este mancha 
+el ambito global de nuestro codigo, por lo tanto es mejor
+idea no utilizarlo 
+
+
+
+LET
+---
+
+Lo mejor que se puede utilizar es let, declarar tus variables
+con let, ya que este tiene nambito reducido el cual no puede
+salir de el bloque "{}"
+
+CONST
+-----
+
+Las constantes como su nombre lo indica, es una manera de declarar valores / variables 
+los cuales no van a cambiar durante todo el codigo.
+Solo funciona con valores primitivos, -string - numbers - booleans
+Si lo hacemos con valores compuestos esto no funcionara. 
+
+const OBJETO = {
+    nombre= "alex", edad = 19
+}
+
+const VALORES = ["blanco","Negro","Azul"]
+
+objeto.correo = "alex@gmail.com"; (sirve para agregar algo a un objeto)
+colores.push ("anaranjado") (para agregar un elemento a un array)
+
+
+
+
+
+*/
