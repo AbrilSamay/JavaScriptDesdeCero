ed26f421f0d4f9c3c1fca93cff6218a58ef99b7b
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2023-09-24 14:37:21 -0600

    Strings

---
+++ JavaScript desde cero/tipos de datos/Strings/strings.js
@@ -0,0 +1,22 @@
+/*
+STRINGS 
+-------
+
+Son cadenos de texto, hay varias maneras de declararlos. 
+
+let nombre = "Alex"   (comimllas dobles)
+let apellido = 'Romana'  (comillas simples)
+let saludo = new string ("Hola mundo")
+let lorem = "lorem ipsum, dolor sit amet"
+
+
+
+FUNCIONES A RECORDAR 
+
+nombre.length (cuenta los caracteres)
+nombre.toUpperCase ()     =   //Coloca todo en mayusculas
+apellido.loLowerCase ()  //coloca todo en minusculas 
+lorem.includes ("amet")     // Busca "amet" en un texto
+lorem.trim (" ")   // Separa quita los espacios en blanco, sobrantes en un texto 
+lorem. split (",") // Separa un texto por lo que gustes usar
+*/
