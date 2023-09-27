79e8dcf1ac0daab1baf694b10c1e908177575fb4
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2023-09-26 19:36:39 -0600

    error var let y const fixed

--- JavaScript desde cero/tipos de datos/Let, var y const/Var_let_y_const.js
+++ JavaScript desde cero/tipos de datos/Let, var y const/Var_let_y_const.js
@@ -10,8 +10,11 @@ Usar var es una muy mala practica debido que este mancha
 el ambito global de nuestro codigo, por lo tanto es mejor
 idea no utilizarlo 
 
+*/
+var varibleGlobal = "Esto aparecera en el ambito global"
 
 
+/*
 LET
 ---
 
@@ -19,6 +22,10 @@ Lo mejor que se puede utilizar es let, declarar tus variables
 con let, ya que este tiene nambito reducido el cual no puede
 salir de el bloque "{}"
 
+*/
+let mejorManeraDeDeclarar = "Siempre trata de usar let"
+
+/*
 CONST
 -----         
 
@@ -26,15 +33,14 @@ Las constantes como su nombre lo indica, es una manera de declarar valores / var
 los cuales no van a cambiar durante todo el codigo.
 Solo funciona con valores primitivos, -string - numbers - booleans
 Si lo hacemos con valores compuestos esto no funcionara. 
-
-const OBJETO = {
-    nombre= "alex", edad = 19
+*/
+const objeto = {
+    nombre : "alex", edad : 19
 }
 
 const VALORES = ["blanco","Negro","Azul"]
 
-objeto.correo = "alex@gmail.com"; (sirve para agregar algo a un objeto)
-colores.push ("anaranjado") (para agregar un elemento a un array)
+objeto.correo = "alex@gmail.com";  /*(sirve para agregar algo a un objeto)*/
+colores.push ("anaranjado") /*(para agregar un elemento a un array)*/
 
 
-*/
