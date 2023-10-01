16b583ce9456407ceb3c1ae97aa411bd4c2e17c9
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2023-10-01 14:16:50 -0600

    funciones 1.0

---
+++ JavaScript desde cero/tipos de datos/funciones/funciones.js
@@ -0,0 +1,12 @@
+// La manera mas sencilla de declarar funciones es atraves de funciones declaradas.
+
+// usa la palabra reservada "function" para ser declarada.
+
+function unaFuncionQueDevuelveValor (){
+    console.log ("uno");
+    // return 19; // Una vez efectuado un return el resto de codigo deja de ejecutarse.
+    console.log ("dos")
+    console.log ("tres")
+    return "Este es un texto"
+
+}
