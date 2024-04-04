f9b09573d607821fc9be1d802c3f76c7f059e3ec
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-04-04 14:59:15 -0600

    algunas mejoras

---
+++ JavaScript desde cero/tipos de datos/Arrays/arrays.js
@@ -0,0 +1,22 @@
+/*
+Podemos usar const en los arreglos, ya que este no da problama is 
+el contenido llegase a cambiar.
+*/
+
+// ARREGLOS: []
+// Un array no es mas que una coleccion de elementos
+
+const a = []; // Este es un arreglo vacio. 
+
+const b = [1,true,"hola",["a","b","c"]]; // SI un array puede tener otro array en su interior. 
+
+// No pasa nada si guardas un array dentro de otro array 
+
+console.log (b)
+console.log ("legth: es el numero de elementos en el array")
+
+const colores = [];
+
+colores.push = ["rojo", "azul", "verde"] // push agrega elementos al array (siempre lo agrega al ultimo.)
+colores.pop = (); // Elimina un elemento del array
+
