09ac525a3a0d811e5559aaf45b4d30c4c0401575
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-04-15 16:04:35 -0600

    he olvidado esto, lo retomare ahora

--- JavaScript desde cero/tipos de datos/1 Let, var y const/Var_let_y_const.js
+++ JavaScript desde cero/tipos de datos/1 Let, var y const/Var_let_y_const.js
@@ -10,11 +10,23 @@ Usar var es una muy mala practica debido que este mancha
 el ambito global de nuestro codigo, por lo tanto es mejor
 idea no utilizarlo 
 
+Ademas que var no respeta ambito de bloque
+
 */
 var varibleGlobal = "Esto aparecera en el ambito global"
 
+var musica = "Rock"
+console.log("Variable musica antes de iniciar el bloque:",musica)
+//Valor de musica ahora es rock
+ {
+    var musica = "pop"
+    console.log("Variable de musica dentro del bloque:", musica)
+ }
 
-/*
+console.log ("Variable musica despues del bloque: ", musica)
+console.log ( "var no respeta ambito de bloque por ende es una mala practica usarlo")
+ 
+ /*
 LET
 ---
 
@@ -38,7 +50,7 @@ const objeto = {
     nombre : "alex", edad : 19
 }
 
-const VALORES = ["blanco","Negro","Azul"]
+const colores = ["blanco","Negro","Azul"]
 
 objeto.correo = "alex@gmail.com";  /*(sirve para agregar algo a un objeto)*/
 colores.push ("anaranjado") /*(para agregar un elemento a un array)*/
