a024eca86b5730444071b2de42dc270a3fcd1d24
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-06 16:30:54 -0600

    function 3

--- JavaScript desde cero/tipos de datos/7 funciones/funciones.js
+++ JavaScript desde cero/tipos de datos/7 funciones/funciones.js
@@ -7,6 +7,20 @@ function unaFuncionQueDevuelveValor (){
     // return 19; // Una vez efectuado un return el resto de codigo deja de ejecutarse.
     console.log ("dos")
     console.log ("tres")
-    return "Este es un texto"
+    return "Esto es lo que retorna la varible"
 
+}
+
+let valorDeLaFunction = unaFuncionQueDevuelveValor ();
+
+// una fuction puede retornar otra fuctino 
+
+function funcionQueDevuelveFuncion (){
+    console.log("esta es la primera funcion, esta no hace nada mas que devolver un texto en la consola")
+    return function funcionRetornada () {
+        console.log('esto es rotornado por la segunda funcion retornada desde la primera funcion ')
+        return function terceraFuncionRetordada (){
+            console.log(`todo esto viene desde la tercera funcicion retornada desde la segunda `)
+        }
+    }
 }
