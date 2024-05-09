8dcec494db52f51afbe37ebec436fede49b311c3
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-09 16:03:54 -0600

    arrays 2

--- JavaScript desde cero/tipos de datos/8 Arrays/Arrayss.js
+++ JavaScript desde cero/tipos de datos/8 Arrays/Arrayss.js
@@ -17,7 +17,14 @@ const b = [1,true,"hola",["a","b","c"]]; // SI un array puede tener otro array e
 console.log (b)
 console.log ("legth: es el numero de elementos en el array")
 
-const colores = [];
+const colores = ['hola','elemento'];
+colores.push ('verde',"los elementos que gustes",[1,2,3])
 
-colores.push = ["rojo", "azul", "verde"] // push agrega elementos al array (siempre lo agrega al ultimo.)
-colores.pop = (abc) // Elimina un elemento del array
+colores.push = ['rojo','verde','azul'] // push agrega elementos al array (siempre lo agrega al ultimo.)
+
+//colores.pop = ('rojo') // Elimina un elemento del array
+console.log(colores,'colores')
+console.log (colores)
+
+const arrayEjemplo = Array(100).fill(true)
+console.log(arrayEjemplo,'este es el array que estoy buscando')
