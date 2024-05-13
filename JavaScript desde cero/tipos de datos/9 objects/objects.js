b5be4a02938a5e5ea4c2f866f516e93d5e3fd0ae
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-13 15:45:20 -0600

    objects 1.1

--- JavaScript desde cero/tipos de datos/9 objects/objects.js
+++ JavaScript desde cero/tipos de datos/9 objects/objects.js
@@ -1,1 +1,9 @@
 // objectos en javascript 
+
+
+/*los objetos son una coleccion de "llave"-"valor" algo similar a las 
+propiedas de CSS  
+
+En resumen, son llaves con valores */
+
+// EJEMPLO DE UN OBJETO
