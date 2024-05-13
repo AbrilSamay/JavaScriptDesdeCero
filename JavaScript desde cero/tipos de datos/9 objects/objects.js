0e47cdb542f6dd5987e03721eecd2d018cd5d544
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-13 15:52:49 -0600

    objects 1.2

--- JavaScript desde cero/tipos de datos/9 objects/objects.js
+++ JavaScript desde cero/tipos de datos/9 objects/objects.js
@@ -7,3 +7,21 @@ propiedas de CSS
 En resumen, son llaves con valores */
 
 // EJEMPLO DE UN OBJETO
+
+const Jota = {
+    nombre:'alexis',
+    apellido: 'romana',
+    edad: 20,
+    altura: 1.66,
+    peso: '70 kg',
+    talla: 'M',
+    pasatiempos: [`programar`,'leer', 'tocar violin'],
+    soltero: true,
+    contacto: {whatsapp: 87989801,
+               email: 'alexisjosueromanaenamorado@gmail.com',
+               facebook: 'Alexis Romana'
+    },
+    saludar: function (nombre,apellido){
+        console.log(`Hola de nuevo ${nombre} ${apellido}, espero todo vaya de maravilla`)
+    }
+}
