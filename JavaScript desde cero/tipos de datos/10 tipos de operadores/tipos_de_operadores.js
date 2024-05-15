ff4bebf521a36734d244b6b4f18b4dc93ceb9211
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-15 15:51:02 -0600

    operadores aritmeticos

---
+++ JavaScript desde cero/tipos de datos/10 tipos de operadores/tipos_de_operadores.js
@@ -0,0 +1,18 @@
+/* tipos de operadores en JavaScript  */
+
+
+// TENEMOS PRINCICALMENTE LOS OPERADORES ARITMETICOS 
+
+/*
++ mas, - menos, * multiplicacion, / divicion, % modulo, () signos de agrupacion.
+*/
+
+// JAVASCRIPT RESTEPA LOS EL ORDEN DE LOS SIGNOS, LEY DE LOS SIGNOS 
+
+let a = 5 + (5-10)*3;
+console.log (a)
+
+let modulo = 5 % 2; // Esto nos da el residuo de una multiplicacion 
+let diivcion = 5 / 2 // Nos da el resultado de una divicion 
+console.log (`Este es el resultado de la vicicion 5/2 ${diivcion}
+Este es el residuo de la divicon 5/2 #${modulo}`)
