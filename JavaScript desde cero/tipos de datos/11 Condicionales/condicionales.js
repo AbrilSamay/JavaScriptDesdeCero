d92fd31bf22b398139c6b3fed189f6bfddbdc931
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-22 20:17:38 -0600

    condicionales 1.1

--- JavaScript desde cero/tipos de datos/11 Condicionales/condicionales.js
+++ JavaScript desde cero/tipos de datos/11 Condicionales/condicionales.js
@@ -1,1 +1,23 @@
 //Condicionales
+
+
+/*
+Hasta el momento hemos estado usando estructuras secueciales ya que se ejecuta una linea despeus de la otra 
+
+- Tambien existen estructuras condicionales dependiendo de la evaluacion de una condicion hacemos ciertas
+ acciones u otras
+
+*/
+
+
+//ESTRUCTURAS CONDICIONALES
+
+//if y else, funcionan de tal manera que si una condicion se cumple se ejecturan ciertas acciones.
+
+let edad = 17;
+
+if (edad > 17){
+    console.log ("Eres mayor de edad")
+} else {
+    console.log ("Eres menor de edad")
+}
