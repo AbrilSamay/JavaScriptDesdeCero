aa87356a1fe350f55caa6bc7049c2e4a06e5080e
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-29 18:06:12 -0600

    while y do while

--- JavaScript desde cero/tipos de datos/12 Loops/loops.js
+++ JavaScript desde cero/tipos de datos/12 Loops/loops.js
@@ -3,8 +3,34 @@
 
 console.log ('testing ')
 
-let contador = 0;
+let contador = 7;
 
-while (contador > 10){
-    console.log(contador)
+
+//Con  while, primero se evaluraa la condicion relacional ante de ejecutar el codigo.
+
+while (contador < 10){
+    console.log ('while '+contador)
+    contador++;
 }
+
+// Con do while primero se ejecuta el codigo y despues evalua la condicion 
+// es decir no importa si la condicion no se cumple el codigo se ejecutara siempre una vez 
+
+
+
+  do{
+    console.log('Do while '+ contador)
+    contador ++;
+ }while (contador < 10)
+
+  // Tanto como while y do while han quedadon en el pasado, ahora en dia lo que se usa es for y sus variantes.
+
+
+ let array = [10,20,30,40];
+
+
+
+  for (let index = 0; index < array.length; index++) {
+    console.log(array[index])
+    
+  }
