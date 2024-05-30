fed57664dcc2a7e04241c734b4707a51e51e34f3
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-05-30 17:18:51 -0600

    loops for in, for of

--- JavaScript desde cero/tipos de datos/12 Loops/loops.js
+++ JavaScript desde cero/tipos de datos/12 Loops/loops.js
@@ -26,11 +26,68 @@ while (contador < 10){
   // Tanto como while y do while han quedadon en el pasado, ahora en dia lo que se usa es for y sus variantes.
 
 
- let array = [10,20,30,40];
+ let array = [10,20,30,40,50,60];
+index = 0
+ let valor = 0
+ 
+ // El mejor metodo que se puede usar hoy en dia en el metodo for
+ /*
+ For cumple mejor las exigencian de do y do while, ya que crea una variable especifica dentro del mismo metodo,
+ nos permite comparar dentro del metodo y incrementar y decremenatar.
+ */
 
-
-
-  for (let index = 0; index < array.length; index++) {
+   for (let index = 0; index < array.length; index++) {
     console.log(array[index])
-    
   }
+
+
+
+  const objetoPerron = {
+    nombre: 'alex',
+    apellido: 'romana',
+    edad: 20
+  }
+
+  //Existen varios tipos de metodos For, algunos de ellos son:
+
+  /*
+  FOR IN 
+  ------  
+  el for in es un loop que nos permite recorrer o iterar las propiedades de un objeto, 
+  se puede decir que es un for especial para los objetos en javascript 
+  */
+
+
+for (const key in objetoPerron) {
+  console.log (`key ${key} valor ${objetoPerron[key]}`)
+}
+  
+/**
+ lo que hara es que en cada loop 'key' tomara el valor de la referencia en el objeto (keys) 
+for in solo te deja recorrer propiedades de un objeto.
+fon in es mas para objetos primitivos en javascript
+
+ */
+
+
+/**
+ FOR OF 
+ ------
+ 
+  Este metodo permite recorrer todos los elementos de cualquier objeto que 
+  sea iterable en javascript
+
+  for of es mas para arrays, strings, boleans y todo en javascript 
+
+ */
+
+  const cualquierArreglo = [1,2,3,4,5]
+  for (const elemento of cualquierArreglo) {
+    console.log (elemento)
+  }
+
+  let string = 'hola mundo'
+   for (const caracter of string) {
+    console.log(caracter)
+    
+   }
