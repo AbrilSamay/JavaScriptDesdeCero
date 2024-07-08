077e679bd43d9985360f673d8c08bf72051c9296
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-08 15:36:01 -0600

    mothod sqrt,pow,round,flooc,cailng

--- JavaScript desde cero/3 Objetos y funciones/3 objeto Math/math.js
+++ JavaScript desde cero/3 Objetos y funciones/3 objeto Math/math.js
@@ -11,6 +11,7 @@ Tambien tiene los valores de algunos digitos como pi = 3.14159265
 
 */
 
+console.log (Math.PI)
 
 console.log (Math)// Asi podremos ver todos los metodos y valores de este prototipo.
 
@@ -24,3 +25,29 @@ vamos a acceder.
 // Math.abs (nos da el valor absoluto de un numero)
 
 console.log (Math.abs(-7.8),'valor absoluto de un numero con Math.abs')
+
+ // Si necesitamos redondear un numero contamos con los siguientes metodos:
+
+ // .ceil  cealing, redondeara hasta el entero superior absoluto( el numero mas alto cercano);
+
+ console.log (Math.ceil(7.2),'7.2 siendo redondeado al entero mayor absoluto') // no importa si esta en lo minimo, lo subira a la maximo.
+
+ // Si necesitamos hacer lo contracio tenemos el method floow a nuestra dispocision.
+
+ console.log (Math.floor(7.9), '7.9 siendo redondeado hasta 7 por el metodoo floor')
+ // el method floor llevara nuestro decimal al el numero entero inferior absoluto.
+
+ // Ahora para hacerlo de la manera tradicional utilizamos el metodo round 
+
+ console.log (Math.round(8.8),'8.8 siendo redondeado a 9') // redondea al valor mas cercano.
+ console.log (Math.round(8.3),'8.3 siendo redondeado a 8') // redondea al valor mas cercano. 
+
+ 
+ // Si necesitamos calcular una raiz cuadrada contamos con el method 'sqrt' = Square Root 
+
+ console.log (Math.sqrt(81),'raiz cuadrada de 81, 9') // Calcula la raiz cuadrada de 81 que es 9
+ 
+ 
+//  A su vez podemos elevar numeros a una potencia con el method power 
+
+console.log (Math.pow(2,5)) // primero colocamos el numero a elevar, para despues colocar el exponente.
