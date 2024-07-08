dd13aa2778587065f17d450f10c109e7a8a57ff3
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-08 15:08:13 -0600

    objeto Math

---
+++ JavaScript desde cero/3 Objetos y funciones/3 objeto Math/math.js
@@ -0,0 +1,26 @@
+/*
+Math Object 
+-----------
+
+
+
+Math es un objeto estatico es decir tendremos que que invocar al 
+prototipo de este objeto. 
+Tiene muchos methods bastante utiles como para calcuar seno, coseno, hipotenusa y otros mas 
+Tambien tiene los valores de algunos digitos como pi = 3.14159265
+
+*/
+
+
+console.log (Math)// Asi podremos ver todos los metodos y valores de este prototipo.
+
+/*
+Como ya he dicho este prototipo contiene varios metodos y valores bastantes utiles a la hora 
+de programar, ya sean como para calcular cosenos, valores de pi etc 
+Para acceder a estos methods y valores colocamos un punto y el method/valor al cual 
+vamos a acceder.
+*/
+
+// Math.abs (nos da el valor absoluto de un numero)
+
+console.log (Math.abs(-7.8),'valor absoluto de un numero con Math.abs')
