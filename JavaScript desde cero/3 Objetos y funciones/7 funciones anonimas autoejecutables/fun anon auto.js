cf4036fae405ae039fff0b37fd1fdb06d2af9d99
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-12 16:47:19 -0600

    funcion anonima autoejecutable

---
+++ JavaScript desde cero/3 Objetos y funciones/7 funciones anonimas autoejecutables/fun anon auto.js
@@ -0,0 +1,15 @@
+/*
+Funcion anonima autoejecutable 
+-------------------------------
+
+Una funcion anonima auto ejecutable es una funcion en la que se engloba todo el codigo que se 
+quiere ejecutar.
+Es una manera de que cuando tu archivo carga en el navegador, se ejecuta la programacion que 
+esta en ese archivo.
+Tambien esta protegido de efectos secundarios que pudiera tener la invocacion de librerias 
+de terceros o mismos archivos de tu propiedad. 
+Ademas permite cambiar el contexto para que puedas tener variabels con el mismo nombre.
+
+una funcion con nombre es evidentemente una funcion nombrada y una no nombrada no tiene nombre.
+
+*/
