02cd299f69f4657459190f5e47df3fc0c762ba1e
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-19 16:39:14 -0600

    ejercicion parte 1.2

--- JavaScript desde cero/4 ejercicios programacion/1 PRIMER EJERCICIO/primerEjercicio.js
+++ JavaScript desde cero/4 ejercicios programacion/1 PRIMER EJERCICIO/primerEjercicio.js
@@ -48,15 +48,20 @@ const devorArray = function (string,valor){
 
 devorArray ('hola que tal'," ");
 
-/
+/*
+4) Programa una función que repita un texto X veces,
+ pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+*/
+
 console.log (`4) Programa una función que repita un texto X veces, 
 pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.`);
 
-const numeroVeces = function (string,numero){
-
-
-
-    console.log (string,numero)
+const numeroVeces = function (string,valor1){
+    console.log ( `el texto ${string}, se mandara a la consola ${valor1} de veces`)
+    for (let i = 1; i < valor1; i++) { 
+        console.log (string,i);
+    }
+    console.log (string, valor1);
 }
 
-numeroVeces('HOLA ',4)
+numeroVeces('HOLA ',9)
