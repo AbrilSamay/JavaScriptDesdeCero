098b8d0546db6964eef070cff4bccdb8c045dfd7
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-23 15:54:07 -0600

    segundo ejercicio

---
+++ JavaScript desde cero/4 ejercicios programacion/2 SEGUNDO EJERCICIO/segundo ejercicio.js
@@ -0,0 +1,15 @@
+/*
+2) Programa una función que te devuelva el texto recortado según el 
+número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+*/
+
+console.log (`2) Programa una función que te devuelva el texto recortado según el 
+número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".`)
+
+const cortarCaracteres = function (string,numeroCortar){
+    let nuevo = string.substring(0,numeroCortar);
+    console.log (`el texto: ${string} se cortara en: ${numeroCortar} quedando: ${nuevo}`);
+    return nuevo
+}
+
+console.log (cortarCaracteres('Hola Mundo',4));
