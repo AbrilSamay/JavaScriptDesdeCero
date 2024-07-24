3da5dc01281720f77987e223c68528bf6d92cba1
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-24 17:11:04 -0600

    segundo ejercicio complete

--- JavaScript desde cero/4 ejercicios programacion/2 SEGUNDO EJERCICIO/segundo ejercicio.js
+++ JavaScript desde cero/4 ejercicios programacion/2 SEGUNDO EJERCICIO/segundo ejercicio.js
@@ -3,15 +3,50 @@
 número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 */
 
-console.log (`2) Programa una función que te devuelva el texto recortado según el 
-número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".`)
 
-const cortarCaracteres = function (string,numeroCortar){
-    let nuevo = string.substring(0,numeroCortar);
-    console.log (`el texto: ${string} se cortara en: ${numeroCortar} quedando: ${nuevo}`);
-    return nuevo
+
+let expRegInputValue =  /[^a-zA-Z\s,]/; // special characters 
+let inputValue = prompt ('ingrese el texto a cortar ');
+let inputValueTrim = inputValue.trim();
+let expRegInputValueResult = expRegInputValue.test (inputValue)
+let numeroCaracteres = inputValueTrim.length;
+let expRegInputNumberValue = /[^0-9]/; // numbers only
+
+if (!inputValue) {
+    alert ('ingresa un valor');
+    location.reload();
+} else {
+    if (expRegInputValueResult) {
+        console.log ('El texto ingresado no es valido');
+        alert('El texto ingresado no es valido');
+        location.reload();
+    } else {
+        let contiueOrStop = confirm (`el texto ingresado es "${inputValueTrim}" deseas cortarlo?`);
+        if (!contiueOrStop) {
+            alert('operacion cancelada')
+        } else {
+            while (true){
+               let cut = prompt ('Numero de caracteres a cortar'); 
+               let cutTrim = cut.trim()
+               let cutResultExpReg = expRegInputNumberValue.test (cutTrim);
+               console.log (cutTrim)
+               if (cutResultExpReg){
+                   alert('ingresa un numero');
+                   continue;
+               } else {
+                console.log ('se esta ejecutando esto ?')
+                   function cortarCaracteres (cutTrim){
+                       let result = inputValueTrim.substring (0, cutTrim);
+                       alert (`el resultado es ${result}`);
+                    } 
+                    cortarCaracteres (cutTrim);
+                    break;
+               }
+           }
+        }
+    }
 }
 
-console.log (cortarCaracteres('Hola Mundo',4));
-
+//console.log (`2) Programa una función que te devuelva el texto recortado según el 
+//número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".`)
 
