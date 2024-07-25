ed32ec763cdec736b8e2812e26a111b5762a5f77
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-25 14:39:08 -0600

    tercer ejericio test

--- JavaScript desde cero/3 Objetos y funciones/6 Expresiones regulares/test.js
+++ JavaScript desde cero/3 Objetos y funciones/6 Expresiones regulares/test.js
@@ -1,22 +1,5 @@
 
 
-/*
-3) Programa una función que dada una String te devuelva un Array de textos
- separados por cierto caracter, pe. miFuncion('hola que tal', ' ') 
- devolverá ['hola', 'que', 'tal'].
-*/
-
-console.log (`3) Programa una función que dada una String te devuelva un Array de textos
- separados por cierto caracter, pe. miFuncion('hola que tal', ' ') 
- devolverá ['hola', 'que', 'tal'].`)
-
-const devorArray = function (string,valor){
-    let partir = string.split (valor)
-    console.log (`el texto ${string} dividido por sus espacios seria: ${partir}`)
-    console.log (partir);
-};
-
-devorArray ('hola que tal'," ");
 
 /*
 4) Programa una función que repita un texto X veces,
