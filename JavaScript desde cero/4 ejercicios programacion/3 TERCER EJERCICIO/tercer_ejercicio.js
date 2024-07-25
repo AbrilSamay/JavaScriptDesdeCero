3b3477feb7bf8fab02de2b2dad0749ad95424be8
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-25 15:10:51 -0600

    segundo ejercico testing

--- JavaScript desde cero/4 ejercicios programacion/3 TERCER EJERCICIO/tercer_ejercicio.js
+++ JavaScript desde cero/4 ejercicios programacion/3 TERCER EJERCICIO/tercer_ejercicio.js
@@ -6,6 +6,29 @@
 */
 
 
+while (true) {
+
+let expRegInput = /[^a-zA-Z\s]/;
+let input = prompt ('ingresa el texto a dividir por espacios');
+let inputTrim = input.trim ();
+let inputExpReg = expRegInput.test (inputTrim);
+
+    if (inputExpReg || !input) {
+        alert ('ingresa un texto valido');
+        continue;
+    } else {
+        let firstConfim = confirm (`el texto: "${inputTrim}" sera cortado en  ESPACIOS deseas continuar?`);
+        if (!firstConfim) {
+            alert('operacion cancelada');
+        } else {
+            let split = inputTrim.split (" ");
+            alert (`el texto "${inputTrim}" dividido por espacios resulta ${split} `);
+        }
+        break;
+    }
+}
+
+
 console.log (`3) Programa una función que dada una String te devuelva un Array de textos
  separados por cierto caracter, pe. miFuncion('hola que tal', ' ') 
  devolverá ['hola', 'que', 'tal'].`)
