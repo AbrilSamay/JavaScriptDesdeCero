eb8d57168cfc7c4b699a7b1db9a2eb93dfaf0c11
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-26 16:38:57 -0600

    cuarto ejercicio issue fixing

--- JavaScript desde cero/4 ejercicios programacion/4 CUARTO EJERCICIO/cuerto_ejercicio.js
+++ JavaScript desde cero/4 ejercicios programacion/4 CUARTO EJERCICIO/cuerto_ejercicio.js
@@ -2,9 +2,8 @@
 
 /*
 4) Programa una función que repita un texto X veces,
- pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+ pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo
 */
-
 
 
 while (true){
@@ -45,3 +44,4 @@ while (true){
         }break;
     }
 };
+
