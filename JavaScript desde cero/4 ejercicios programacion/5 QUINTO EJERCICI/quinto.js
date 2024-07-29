23ab5d09b0886c2b997ae610a27cfea0b65b75a6
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-07-29 16:31:14 -0600

    testing quinto

--- JavaScript desde cero/4 ejercicios programacion/5 QUINTO EJERCICI/quinto.js
+++ JavaScript desde cero/4 ejercicios programacion/5 QUINTO EJERCICI/quinto.js
@@ -1,4 +1,5 @@
-//5) Programa una función que invierta las palabras de una cadena de texto
+//5) Programa una función que invierta 
+//las palabras de una cadena de texto
 //, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
 
 
@@ -8,14 +9,21 @@
 // join (join a string together);
 
 
-let string = 'hola mama';
-console.log (`"${string}" input`)
-const upsideDown = (string) => {
-    let result = string.split ('').reverse ().join ('');
+let expRegInput = /[^a-zA-Z\s,]/;  // a-z characters 
+let input = prompt('ingrese el texto').trim();
+let expRegInputResult = expRegInput.test (input);
+
+const upsideDown = (input) => {
+    let result = input.split ('').reverse ().join ('');
     console.log (`"${result}" output`);
+    alert (`el texto: : "${input}" al reves es: ${result}`)
 };
-upsideDown (string);
+
+if (expRegInputResult) {
+    alert('ingrese un valor valido');
+} else {
+    upsideDown (input);
+};
 
 
 
-let expRegInput = /[^a-zA-Z\s]/; 
