584be70ef61477696086bd9183f31c82a36a3f41
AbrilSamay <76850828+AbrilSamay@users.noreply.github.com>
2024-08-02 15:51:46 -0600

    septimo ejercicio testing

--- JavaScript desde cero/4 ejercicios programacion/7 SEPTIMO EJERCICIO/septimo.js
+++ JavaScript desde cero/4 ejercicios programacion/7 SEPTIMO EJERCICIO/septimo.js
@@ -1,1 +1,27 @@
 // septimo ejercicio
+/*
+6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, 
+pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+*/
+
+
+let text = prompt ('ingrese texto en el que se buscara');
+let input = prompt ('ingrese texto el cual buscar');
+
+const toSearch = (text,input) => {
+    let counter = 0;
+    let value = 0;
+
+    while( value !== -1){
+        value = text.indexOf(input, value);
+
+        if (value !== -1){
+            counter++;
+            value++;
+        }
+
+    }
+    alert (`Se encontraron: "${counter} ${input}" en el texto "${text}"`)
+} 
+
+if (!text || !input) {'no se han ingresado algunos valores'} else { toSearch (text,input)};
