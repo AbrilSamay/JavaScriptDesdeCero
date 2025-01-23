b4c22b3f187fec3b3446eebefc2988fe76ee34e3
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-01-22 19:41:03 -0600

    Ejercicio 25

---
+++ JavaScript desde cero/4 ejercicios programacion/25 eleminar duplicados/eleminarDuplicados.js
@@ -0,0 +1,17 @@
+
+/*
+25) Programa una función que dado un arreglo de elementos,
+ elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
+  devolverá ["x", 10, 2, "10", true].
+*/
+
+let input = [1,2,3,4,5,5]; // input donde se buscara.
+
+
+const duplicadosBuscar = (input) => {
+  const filtered = input.filter ((callBack,index,self) => self.indexOf (callBack) !== index);
+  return (console.log (`Array original: ${input} \nArray filtrado: ${filtered}`));
+} 
+
+
+console.log (duplicadosBuscar(input))
