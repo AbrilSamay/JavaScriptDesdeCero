866e18c223f8e85a1e472de2b7826e48c5ac45a3
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-01-24 21:49:14 -0600

    26 practica reduce method

--- JavaScript desde cero/4 ejercicios programacion/26_promedio/calcularPromedio.js
+++ JavaScript desde cero/4 ejercicios programacion/26_promedio/calcularPromedio.js
@@ -1,6 +1,6 @@
 // 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
 
-const palabras = [2,3,4,5,6,7,8];
+const numeros = [2,3,4,5,6,7,8];
 
 
 const promedio = (array) => {
@@ -14,6 +14,19 @@ const promedio = (array) => {
     return console.log (`El promedio de: ${array} es: ${suma}`)
 }
 
-promedio (palabras);
+promedio (numeros);
 
 // se debe de hacer con reduce y join
+
+// Practica reduce 
+
+const palabras = ['Hola', 'mundo', 'lindo'];
+
+const frase = palabras.reduce((acumulador, valorActual) => {
+  return acumulador + ' ' + valorActual; // Concatenamos palabras con un espacio
+}, '');
+
+console.log(frase); // Salida: " Hola mundo lindo"
+
+
+// mas o menos entiendo
