ff4fedeeb090bd4b5239051ec104be7bbf92e7da
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-01-30 20:46:00 -0600

    ejercico 27 html

--- JavaScript desde cero/4 ejercicios programacion/27_big_assigment/27.js
+++ JavaScript desde cero/4 ejercicios programacion/27_big_assigment/27.js
@@ -25,3 +25,5 @@ La clase recibirá un objeto al momento de instanciarse con los siguentes datos:
     Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
 
 */
+
+// no tengo idea de como hacer esto xd
