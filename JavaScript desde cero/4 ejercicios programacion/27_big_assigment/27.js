8feaac8ed3ef338cead2905ec4ae17ce7b6500d6
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-02-12 18:17:46 -0600

    static methods

--- JavaScript desde cero/4 ejercicios programacion/27_big_assigment/27.js
+++ JavaScript desde cero/4 ejercicios programacion/27_big_assigment/27.js
@@ -10,15 +10,12 @@ La clase recibirá un objeto al momento de instanciarse con los siguentes datos:
         Valida que el director no rebase los 50 caracteres.
         Valida que el año de estreno sea un número entero de 4 dígitos.
         Valida que el país o paises sea introducidos en forma de arreglo.
-                 Valida que los géneros sean introducidos en forma de arreglo.
-                 Valida que los géneros introducidos esten dentro de los géneros aceptados*.
-
+        Valida que los géneros sean introducidos en forma de arreglo.
+        Valida que los géneros introducidos esten dentro de los géneros aceptados*.
                  Crea un método estático que devuelva los géneros aceptados*.
                  Valida que la calificación sea un número entre 0 y 10 pudiendo ser decimal de una posición.
-
                  Crea un método que devuelva toda la ficha técnica de la película.
                  Apartir de un arreglo con la información de 3 películas genera 3 
-
                  instancias de la clase de forma automatizada e imprime la ficha técnica 
                  de cada película.
 
@@ -27,6 +24,10 @@ La clase recibirá un objeto al momento de instanciarse con los siguentes datos:
 */
 
 class Pelicula {
+
+    static generos(){
+        return console.info (`Los generos aceptados son: ${Pelicula.generosPermitidos.join(", ")}`)
+    }
     constructor ({ id, titulo, director, estreno, paises, genero, calificacion }){
         this.id = this.idValidator ('ID', id);
         this.titulo = this.lenghtCheker ("titulo",titulo, 100) // tiene que ser 100, se usara 5 para test 
@@ -37,6 +38,15 @@ class Pelicula {
         this.calificacion = calificacion;
     }
 
+    
+    static get generosPermitidos () {
+        return [ "Action", "Adult", "Adventure", "Animation", "Biography", "Comedy", "Crime", 
+            "Documentary", "Drama", "Family", "Fantasy", "Film Noir", "Game-Show", 
+            "History", "Horror", "Musical", "Music", "Mystery", "News", "Reality-TV", 
+            "Romance", "Sci-Fi", "Short", "Sport", "Talk-Show", "Thriller", "War", "Western"]
+        
+    }
+   
     /*
                     MULTIUSO FUNCIONES 
                     ------------------
@@ -130,7 +140,8 @@ class Pelicula {
 }
 
 
-
+console.log (Pelicula.generosPermitidos);
+Pelicula.generos();
 
 const peliTest = new Pelicula ({
     id: 'xx1234567',
@@ -138,7 +149,7 @@ const peliTest = new Pelicula ({
     director: "Alex Merito",
     estreno: '2004', 
     paises: ["Honduras"],
-    genero: [' action', 'animation', 'pan', 'comedy', 'algo random'],
+    genero: [' action', 'animation', 'comedy'], 
     calificacionIMBD: 'ni perra idea'
 })
 
