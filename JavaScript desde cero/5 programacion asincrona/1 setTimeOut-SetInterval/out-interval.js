897975a8e87163595c0646c48adf244022252116
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-02-17 17:19:32 -0600

    setInterval

--- JavaScript desde cero/5 programacion asincrona/1 setTimeOut-SetInterval/out-interval.js
+++ JavaScript desde cero/5 programacion asincrona/1 setTimeOut-SetInterval/out-interval.js
@@ -22,3 +22,24 @@ console.warn ('inico contar 4 segundos desde aca')
 setTimeout(() => {
     console.info ("Ejecutando contenido de setTimeout, se ejecuta despues de 4 segundos")
 }, 4000);
+
+
+/*
+Para cuando necesitemos hacer una cosa cada x cantidad de tiempo contamos con 
+setInterval.
+
+el cual como su nombre lo dice, ejecutara una funcion cada n cantidad de tiempo.
+
+*/
+
+
+setInterval(() => {
+    console.info ("ejecutandose este mensaje cada segundo");
+}, 1000);
+
+
+// ejemplo muy simple de un reloj con set interval
+
+setInterval(() => {
+    console.warn (new Date().toLocaleTimeString())
+}, 1000);
