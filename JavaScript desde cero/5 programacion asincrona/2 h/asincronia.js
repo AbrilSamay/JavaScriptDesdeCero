cf8f3088d96e92e5b7dd5b236b6708f8bb771f3a
AprilSamay <alexisjosueromanaenamorado@gmail.com>
2025-02-19 20:54:30 -0600

    asincronia en javascript primeros fundamentos

---
+++ JavaScript desde cero/5 programacion asincrona/2 h/asincronia.js
@@ -0,0 +1,12 @@
+// asincronia en javascript
+
+/*
+La asincronía es uno de los pilares fundamentales de Javascript,
+ ya que es un lenguaje de programación de un sólo subproceso o hilo (single thread), 
+ lo que significa que sólo puede ejecutar una cosa a la vez.
+
+Si bien los idiomas de un sólo hilo simplifican la escritura de 
+código porque no tiene que preocuparse por los problemas de concurrencia,
+ esto también significa que no puede realizar operaciones largas como el
+  acceso a la red sin bloquear el hilo principal.
+*/
